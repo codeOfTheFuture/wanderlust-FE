cd19a3260cc9b43070f7e4fb8f2b1b12b7d759f3
Jeff Oliver <jeffoliver3384@gmail.com>
2020-09-12 05:43:04 -0400

    created .env for env vars

--- wanderlust/src/firebase.js
+++ wanderlust/src/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCca7zM4LtiL7PygucWbisKSzAFLpp8Be0",
-  authDomain: "wanderlust-4a7a5.firebaseapp.com",
-  databaseURL: "https://wanderlust-4a7a5.firebaseio.com",
-  projectId: "wanderlust-4a7a5",
-  storageBucket: "wanderlust-4a7a5.appspot.com",
-  messagingSenderId: "420115187470",
-  appId: "1:420115187470:web:ff29e0a7bf3e36b2d1395f",
-  measurementId: "G-TFLQKXX3K7",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
