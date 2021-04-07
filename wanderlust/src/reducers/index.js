8c34cec386a790fd76bcf3b151d1a5c3988c346d
Jeff Oliver <jeffoliver3384@gmail.com>
2021-04-07 14:32:38 -0400

    first commit

--- wanderlust/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import tourReducer from './toursReducer';
-import userReducer from './userReducer';
-
-export default combineReducers({
-  userReducer,
-  tourReducer
-});
